6af540ff9eca3f6b5271de6213281df7899a9932
emkhanhhocgioi <khanhlv.21it@vku.udn.vn>
2025-04-20 23:11:02 +0700

    buh

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-const http = require('http');
-const port = process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Express!</h1>');
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
